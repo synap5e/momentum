8b82e0c1f9ea30ebd845697e4bc5469908539572
ChrisChin <chinchri1@myvuw.ac.nz>
2015-05-02 20:49:56 +1200

    Add player/camera rotation and reset
    Added ResetRecording to create a new recording
    
    added player/camera rotation to the snapshot

--- Assets/Scripts/Recorder.cs
+++ Assets/Scripts/Recorder.cs
@@ -8,11 +8,9 @@ public class Recorder : MonoBehaviour {
 	public List<Snapshot> snapshotList {get; set;}
 	private bool recording = false;
 
-
 	public GameObject player;
 	private Rigidbody playerRigidbody;
 	private RigidbodyFPSController rigidbodyFPSController;
-
 	private float frameDuration;
 
 	public bool IsRecording
@@ -23,17 +21,22 @@ public class Recorder : MonoBehaviour {
 		}
 	}
 
+
 	public class Snapshot{
 		public Vector3 position{ get; set;}
 		public Quaternion rotation{get; set;}
 		public bool inJump{get; set;}
 		public float duration{ get; set;}
+		public float playerRotation{ get; set;}
+		public float cameraRotation{ get; set;}
 
-		public Snapshot(Vector3 position,Quaternion rotation,bool inJump, float duration){
+		public Snapshot(Vector3 position,Quaternion rotation,bool inJump, float duration,float playerRotation,float cameraRotation){
 			this.position = position;
 			this.rotation = rotation;
 			this.inJump = inJump;
 			this.duration = duration;
+			this.playerRotation = playerRotation;
+			this.cameraRotation = cameraRotation;
 		}
 	}
 
@@ -52,9 +55,11 @@ public class Recorder : MonoBehaviour {
 				Quaternion rotation = playerRigidbody.rotation;
 				bool inJump = rigidbodyFPSController.onGround;
 				float duration = frameDuration;
+				float playerRotation = player.transform.localEulerAngles.y;
+				float cameraRotation = player.GetComponent<Camera>().transform.localEulerAngles.x;
 
 				//Add the snapshot to the list
-				Snapshot s = new Snapshot (position, rotation, inJump, duration);
+				Snapshot s = new Snapshot (position, rotation, inJump, duration,playerRotation,cameraRotation);
 				snapshotList.Add (s);
 
 				//reset frame duration
@@ -70,6 +75,10 @@ public class Recorder : MonoBehaviour {
 
 	public void StopRecording(){
 		recording = false;
+	}
+
+		snapshotList = new List<Snapshot> ();
+		recording = true;
 	}
 
 }
