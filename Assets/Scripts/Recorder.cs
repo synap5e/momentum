2b582713490233947ee48c7eed321f05e6282e23
ChrisChin <chinchri1@myvuw.ac.nz>
2015-05-06 21:32:13 +1200

    added recorder file
    see previous commit

--- Assets/Scripts/Recorder.cs
+++ Assets/Scripts/Recorder.cs
@@ -9,14 +9,13 @@ public class Recorder : MonoBehaviour {
 
 	public float recordingFrameDurationMin;
 	public List<Snapshot> snapshotList {get; set;}
+	public List<KeyRecording> keylist {get; set;}
 	private bool recording = false;
-
 	public bool recordKeys = true;
 	public GameObject player;
 	private Rigidbody playerRigidbody;
 	private RigidbodyFPSController rigidbodyFPSController;
 	private float frameDuration;
-	private String fileName = "Keylog.txt";
 
 	public bool IsRecording
 	{
@@ -26,6 +25,10 @@ public class Recorder : MonoBehaviour {
 		}
 	}
 
+	public Recorder(){
+		keylist = new List<KeyRecording>();
+		snapshotList = new List<Snapshot> ();
+	}
 
 	public class Snapshot{
 		public Vector3 position{ get; set;}
@@ -34,20 +37,32 @@ public class Recorder : MonoBehaviour {
 		public float duration{ get; set;}
 		public float playerRotation{ get; set;}
 		public float cameraRotation{ get; set;}
+		public List<KeyRecording> keyRecList { get; set;}
 
-		public Snapshot(Vector3 position,Quaternion rotation,bool inJump, float duration,float playerRotation,float cameraRotation){
+		public Snapshot(Vector3 position,Quaternion rotation,bool inJump, float duration,float playerRotation,float cameraRotation,List<KeyRecording> keyRecList){
 			this.position = position;
 			this.rotation = rotation;
 			this.inJump = inJump;
 			this.duration = duration;
 			this.playerRotation = playerRotation;
 			this.cameraRotation = cameraRotation;
+			this.keyRecList = keyRecList;
+		}
+	}
+
+	public class KeyRecording{
+		public float time{get; set;}
+		public String type{get; set;}
+		public String key{get; set;}
+		public KeyRecording(float time, String type, String key){
+			this.time = time;
+			this.type = type;
+			this.key = key;
 		}
 	}
 
 	void Start () {
 		snapshotList = new List<Snapshot> ();
-		File.CreateText (fileName);
 		playerRigidbody = player.GetComponent<Rigidbody>();
 		rigidbodyFPSController = player.GetComponent<RigidbodyFPSController>();
 	}
@@ -56,9 +71,6 @@ public class Recorder : MonoBehaviour {
 	void Update(){
 		if (recording) 
 			RecordSnapshot();
-
-		if(recordKeys)
-			RecordKeys ();
 	}
 
 	public void RecordSnapshot(){
@@ -69,28 +81,50 @@ public class Recorder : MonoBehaviour {
 			bool inJump = rigidbodyFPSController.onGround;
 			float duration = frameDuration;
 			float playerRotation = player.transform.localEulerAngles.y;
-			Camera camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+			Camera camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
 			float cameraRotation = camera.transform.localEulerAngles.x;
 			
 			//Add the snapshot to the list
-			Snapshot s = new Snapshot (position, rotation, inJump, duration,playerRotation,cameraRotation);
+			Snapshot s = new Snapshot (position, rotation, inJump, duration, playerRotation, cameraRotation,keylist);
 
 			snapshotList.Add (s);
 			
 			//reset frame duration
 			frameDuration = 0;
-		}
-	}
 
-	public void RecordKeys(){
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			File.AppendAllText(fileName, DateTime.Now.ToString() + " Space" +"\n");
-		}
-		else if (Input.GetKeyDown (KeyCode.LeftShift)) {
-			File.AppendAllText(fileName, DateTime.Now.ToString() + " LeftShift" +"\n");
-		}
-		else if (Input.anyKeyDown) {
-			File.AppendAllText(fileName, DateTime.Now.ToString() + " "+Input.inputString +"\n");
+			//reset keylist
+			keylist = new List<KeyRecording> ();
+		} 
+		else {//Still within frame so record keys
+			if(recordKeys){
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyDown","Space"));
+				}
+				else if(Input.GetMouseButtonDown(0)){
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyDown","MouseButton"));
+				}
+				else if (Input.anyKeyDown) {
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyDown",Input.inputString));
+				}
+				if (Input.GetKeyUp (KeyCode.Space)) {
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyUp","Space"));
+				}
+				else if(Input.GetMouseButtonUp(0)){
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyUp","MouseButton"));
+				}
+				else if (Input.GetKeyUp(KeyCode.W)) {
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyUp","w"));
+				}
+				else if (Input.GetKeyUp(KeyCode.A)) {
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyUp","a"));
+				}
+				else if (Input.GetKeyUp(KeyCode.S)) {
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyUp","s"));
+				}
+				else if (Input.GetKeyUp(KeyCode.D)) {
+					keylist.Add (new KeyRecording(Time.deltaTime,"keyUp","d"));
+				}
+			}
 		}
 	}
 
@@ -100,7 +134,6 @@ public class Recorder : MonoBehaviour {
 
 	public void StopRecording(){
 		recording = false;
-		Save ("save.json");
 	}
 
 	public void ResetRecording(){
@@ -117,10 +150,12 @@ public class Recorder : MonoBehaviour {
 	}
 
 	public void ResetLoggingKeys(){
-		File.CreateText (fileName);
+		keylist = new List<KeyRecording>();
 		recordKeys = true;
 	}
 
+
+	//Creates a new Json file of the Recorder
 	public void Save(String filename){
 		JSONNode N = new JSONClass (); // Start with JSONArray or JSONClass
 		float totalDuration = 0;
@@ -142,24 +177,25 @@ public class Recorder : MonoBehaviour {
 			N ["player recording"] ["frames"] [i] ["inJump"].AsFloat = snap.inJump ? 1:0;
 			N ["player recording"] ["frames"] [i] ["y rotation"].AsFloat = snap.playerRotation;
 			N ["player recording"] ["frames"] [i] ["camera tilt"].AsFloat = snap.cameraRotation;
-//			N ["player recording"] ["frames"] [i] ["key events"] [0] ["time"].AsFloat = 0.008f;
-//			N ["player recording"] ["frames"] [i] ["key events"] [0] ["type"] = "keyup";
-//			N ["player recording"] ["frames"] [i] ["key events"] [0] ["key"].AsInt = 32;
-//			N ["player recording"] ["frames"] [i] ["key events"] [1] ["time"].AsFloat = 0.008f;
-//			N ["player recording"] ["frames"] [i] ["key events"] [1] ["type"] = "keyup";
-//			N ["player recording"] ["frames"] [i] ["key events"] [1] ["key"].AsInt = 32;
+			N ["player recording"] ["frames"] [i] ["numberKeyRec"].AsInt = snap.keyRecList.Count;
+			for(int k =0; k<snap.keyRecList.Count; k++){
+				KeyRecording keyRecording = snap.keyRecList[k];
+				N ["player recording"] ["frames"] [i] ["key events"] [k] ["time"].AsFloat = keyRecording.time;
+				N ["player recording"] ["frames"] [i] ["key events"] [k] ["type"] = keyRecording.type;
+				N ["player recording"] ["frames"] [i] ["key events"] [k] ["key"] = keyRecording.key;
+			}
 		}
 		Debug.Log (N.ToJSON (4));
 		File.WriteAllText(filename,N.ToJSON (4));
 	}
 
+	//Loads Recorder from a  Json file 
 	public void Load(String filename){
 		string text = File.ReadAllText(filename);
 		JSONNode N = JSON.Parse(text);
 		int numberFrames = N ["player recording"] ["number frames"].AsInt;
 		snapshotList = new List<Snapshot> ();
 		for(int i =0; i<numberFrames; i++){
-
 			float duration = N ["player recording"] ["frames"] [i] ["duration"].AsFloat;
 			float playerX = N ["player recording"] ["frames"] [i] ["position"][0].AsFloat;		
 			float playerY = N ["player recording"] ["frames"] [i] ["position"][1].AsFloat;
@@ -174,18 +210,16 @@ public class Recorder : MonoBehaviour {
 			if(N ["player recording"] ["frames"] [i] ["inJump"].AsFloat ==1) inJump = true;
 			float playerRotation = N ["player recording"] ["frames"] [i] ["y rotation"].AsFloat;
 			float cameraRotation = N ["player recording"] ["frames"] [i] ["camera tilt"].AsFloat;
-//			N ["player recording"] ["frames"] [i] ["key events"] [0] ["time"].AsFloat = 0.008f;
-//			N ["player recording"] ["frames"] [i] ["key events"] [0] ["type"] = "keyup";
-//			N ["player recording"] ["frames"] [i] ["key events"] [0] ["key"].AsInt = 32;
-//			N ["player recording"] ["frames"] [i] ["key events"] [1] ["time"].AsFloat = 0.008f;
-//			N ["player recording"] ["frames"] [i] ["key events"] [1] ["type"] = "keyup";
-//			N ["player recording"] ["frames"] [i] ["key events"] [1] ["key"].AsInt = 32;
-
-			Snapshot snap = new Snapshot(position, rotation, inJump, duration,playerRotation,cameraRotation);
+			int numberKeyRec = N ["player recording"] ["frames"] [i] ["numberKeyRec"].AsInt;
+			keylist = new List<KeyRecording> ();
+			for(int k =0; k<numberKeyRec; k++){
+				float time = N ["player recording"] ["frames"] [i] ["key events"] [0] ["time"].AsFloat;
+				String type = N ["player recording"] ["frames"] [i] ["key events"] [0] ["type"];
+				String key = N ["player recording"] ["frames"] [i] ["key events"] [0] ["key"];
+				keylist.Add(new KeyRecording(time,type,key));
+			}
+			Snapshot snap = new Snapshot(position, rotation, inJump, duration,playerRotation,cameraRotation,keylist);
 			snapshotList.Add(snap);
 		}
 	}
-
-
-
 }
