8ca435e9a606f275d39424ac237c6c7d1872b7a3
ChrisChin <chinchri1@myvuw.ac.nz>
2015-05-22 15:39:04 +1200

    added textures
    added fonts

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject image;
+	// Use this for initialization
+	void Start () {
+		image.GetComponent<UnityEngine.UI.Image>().color = new Color(0, 0, 0, 0.90f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		image.GetComponent<UnityEngine.UI.Image>().color = new Color(0, 0, 0, 0.90f);
+	}
+}
