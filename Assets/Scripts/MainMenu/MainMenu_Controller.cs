20c7e45be87b60e9b948448dde2774bc7bc6c642
Regan1397 <Regan1397@hotmail.com>
2015-05-24 00:41:08 +1200

    Full menu cycle
    Just needs buttons ect.

--- Assets/Scripts/MainMenu/MainMenu_Controller.cs
+++ Assets/Scripts/MainMenu/MainMenu_Controller.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenu_Controller : MonoBehaviour {
 
 
-
+	public GameObject Fader;
 	public GameObject momentumTitle;
+	public float fadeSpeed = 5f;
+
+
+
+
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	void Awake () {
+		Fader.gameObject.active = true;
+		Fader = GameObject.Find("ScreenFader");
+	Fader.GetComponent<UnityEngine.UI.RawImage> ().CrossFadeAlpha (0f,.5f, true);
+		}
+
 	// Update is called once per frame
 	void Update () {
 	
@@ -20,10 +28,19 @@ public class MainMenu_Controller : MonoBehaviour {
 		momentumTitle.gameObject.active = false;
 	}
 
-
-	 void TitleOn () {
+	void TitleOn () {
 		momentumTitle.gameObject.active = true;
 	}
+
+
+	void FadeIn () {
+		Fader.GetComponent<UnityEngine.UI.RawImage> ().CrossFadeAlpha (1f,.5f, true);
+	}
+
+	void FadeOut () {
+		Fader.GetComponent<UnityEngine.UI.RawImage> ().CrossFadeAlpha (0f,.5f, true);
+	}
+
 
 
 
---
+++ Assets/Scripts/MainMenu/ScreeenFadeInOut.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class ScreeenFadeInOut : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f;
+
+	private bool sceneStarting = true;
+
+	void Awake () {
+
+	}
+
+	void Update ()
+	{
+
+	}
+
+
+	void FadeToClear () {
+
+	}
+}
