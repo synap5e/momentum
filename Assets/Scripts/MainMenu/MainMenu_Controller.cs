972a6bbcd8e0b8e4115d94da0d177afbdbfc6c8f
Regan1397 <Regan1397@hotmail.com>
2015-05-23 22:16:06 +1200

    First half of intro/mainmenu

---
+++ Assets/Scripts/MainMenu/MainMenu_Controller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu_Controller : MonoBehaviour {
+
+
+
+	public GameObject momentumTitle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	 void TitleOff () {
+		momentumTitle.gameObject.active = false;
+	}
+
+
+	 void TitleOn () {
+		momentumTitle.gameObject.active = true;
+	}
+
+
+
+}
+
+
