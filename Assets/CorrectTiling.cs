6ba98313b6d2bf99780b8bec8e50af7396bdf723
Matt Graham <matthew.graham.nz@gmail.com>
2015-06-20 15:25:13 +1200

    Tiling fixes

--- Assets/CorrectTiling.cs
+++ Assets/CorrectTiling.cs
@@ -4,8 +4,6 @@ using System.Collections;
 [ExecuteInEditMode]
 public class CorrectTiling : MonoBehaviour {
     public Material Material;
-    public bool ApplyRotation = false;
-    public bool ApplyPosition = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,14 +12,6 @@ public class CorrectTiling : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (ApplyRotation)
-        {
-            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
-        }
-        if (ApplyPosition)
-        {
-            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime, Space.World);
-        }
         Matrix4x4 m = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
         GetComponent<Renderer>().material.SetMatrix("_Transform", m);
 	}
